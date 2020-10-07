11b7b2a44cdfff73ae30529d272ecbbc6a68e0b3
Andriy68 <osinskyy.andriy@chnu.edu.ua>
2020-10-07 09:20:16 +0300

    Забувзаляти на гіти

--- laba3/13/Program.cs
+++ laba3/13/Program.cs
@@ -1,12 +1,113 @@
 ﻿using System;
-
-namespace _13
+using System.Collections.Generic;
+namespace task_13
 {
-    class Program
+    class People
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Birthday { get; set; }
+        public People(string name, string surname, string birthday)
+        {
+            Name = name;
+            Surname = surname;
+            Birthday = birthday;
+        }
+    }
+    class Ship
+    {
+        public string Parent { get; set; }
+        public string Child { get; set; }
+        public Ship(string parent, string child)
+        {
+            Parent = parent;
+            Child = child;
+        }
+    }
+    class Prog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<People> people = new List<People>();
+            List<Ship> relationships = new List<Ship>();
+            string[] main = Console.ReadLine().Split(' ');
+            for (int i = 0; i < 20; i++)
+            {
+                string[] s = Console.ReadLine().Split(' ');
+                if (s[0] == "End")
+                {
+                    break;
+                }
+                else
+                {
+                    if (s[1] == "-")//check if will be work with '-'
+                    {
+                        if (s.Length == 4)
+                        {
+                            relationships.Add(new Ship(s[0], s[2] + " " + s[3]));
+                        }
+                        if (s.Length == 3)
+                        {
+                            relationships.Add(new Ship(s[0], s[2]));
+                        }
+                    }
+                    else if (s[2] == "-")
+                    {
+                        if (s.Length == 4)
+                        {
+                            relationships.Add(new Ship(s[0] + " " + s[1], s[3]));
+                        }
+                        if (s.Length == 5)
+                        {
+                            relationships.Add(new Ship(s[0] + " " + s[1], s[3] + " " + s[4]));
+                        }
+                    }
+                    else
+                    {
+                        people.Add(new People(s[0], s[1], s[2]));
+                    }
+                }
+            }
+            Console.WriteLine(" ");
+            for (int i = 0; i < people.Count; i++)
+            {
+                if (main[0] == people[i].Name && main[1] == people[i].Surname || main[0] == people[i].Birthday)
+                {
+                    Console.WriteLine($"{people[i].Name} {people[i].Surname} {people[i].Birthday}");
+                    Console.WriteLine("Parents:");
+                    for (int j = 0; j < relationships.Count; j++)
+                    {
+                        if (relationships[j].Child == people[i].Name + " " + people[i].Surname || relationships[j].Child == people[i].Birthday)
+                        {
+                            for (int k = 0; k < people.Count; k++)
+                            {
+                                if (relationships[j].Parent == people[k].Name + " " + people[k].Surname || relationships[j].Parent == people[k].Birthday)
+                                {
+                                    Console.WriteLine($"{people[k].Name} {people[k].Surname} {people[k].Birthday}");
+                                }
+                            }
+                        }
+                    }
+                    Console.WriteLine("Children:");
+                    for (int j = 0; j < relationships.Count; j++)
+                    {
+                        if (relationships[j].Parent == people[i].Name + " " + people[i].Surname || relationships[j].Parent == people[i].Birthday)
+                        {
+                            for (int k = 0; k < people.Count; k++)
+                            {
+                                if (relationships[j].Child == people[k].Name + " " + people[k].Surname || relationships[j].Child == people[k].Birthday)
+                                {
+                                    Console.WriteLine($"{people[k].Name} {people[k].Surname} {people[k].Birthday}");
+                                }
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
--- laba3/15/Program.cs
+++ laba3/15/Program.cs
@@ -1,12 +1,59 @@
 ﻿using System;
 
-namespace _15
+namespace task_15
 {
-    class Program
+    class Tool : Kvadrat
+    {
+        public int Horizontal { get; set; }
+        public int Vertical { get; set; }
+
+        public Tool(int a, int b)
+        {
+            Horizontal = a;
+            Vertical = b;
+        }
+        public override void Mal()
+        {
+            for (int i = 0; i < Vertical; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < Horizontal; j++)
+                {
+                    if (i == 0 || i == Vertical - 1)
+                    {
+                        Console.Write("-");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.Write("|\n");
+            }
+        }
+    }
+    abstract class Kvadrat
+    {
+        abstract public void Mal();
+    }
+    class Prog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string s1 = Console.ReadLine();
+            if (s1 == "Square")
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                Tool figure = new Tool(a, a);
+                figure.Mal();
+            }
+            else
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+                Tool figure = new Tool(a, b);
+                figure.Mal();
+            }
         }
     }
 }
