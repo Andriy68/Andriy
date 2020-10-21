8d4ba6b9dc7249d86c1a3621b2cbdfdf13bfdad1
Andriy68 <osinskyy.andriy@chnu.edu.ua>
2020-10-21 13:09:49 +0300

    4

--- laba4/4/Program.cs
+++ laba4/4/Program.cs
@@ -1,12 +1,178 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace _4
+public class room
 {
-    class Program
+    public room(int id)
     {
-        static void Main(string[] args)
+        Id = id;
+        Patients = new List<patient>();
+    }
+    public List<patient> Patients { get; private set; }
+    public int Id { get; private set; }
+
+    public void AddPatient(patient patient)
+    {
+        if (Patients.Count >= 3)
         {
-            Console.WriteLine("Hello World!");
+            throw new Exception();
         }
+
+        this.Patients.Add(patient);
+    }
+}
+public class patient
+{
+    public patient(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; private set; }
+}
+public class doc
+{
+    public doc(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Patients = new List<patient>();
+    }
+
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public List<patient> Patients { get; private set; }
+
+    public void AddPatient(patient patient)
+    {
+        Patients.Add(patient);
+    }
+
+    public string GetPatients()
+    {
+        return String.Join(Environment.NewLine, Patients.Select(p => p.Name).OrderBy(p => p));
+    }
+}
+public class dep
+{
+    private List<room> rooms;
+    private string name;
+
+    public dep(string name)
+    {
+        this.name = name;
+
+        this.rooms = new List<room>();
+        for (int i = 0; i < 20; i++)
+        {
+            rooms.Add(new room(i + 1));
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+
+    public void AddPatientToRoom(patient patient)
+    {
+        var room = this.rooms.FirstOrDefault(r => r.Patients.Count < 3);
+
+        if (room != null)
+        {
+            room.AddPatient(patient);
+        }
+    }
+
+    public string GetAllPatients()
+    {
+        var a = this.rooms.SelectMany(r => r.Patients).ToList();
+        return String.Join(Environment.NewLine, this.rooms.SelectMany(r => r.Patients).Select(p => p.Name).ToList());
+    }
+
+    public string GetPatientsByRoom(int id)
+    {
+        return String.Join(Environment.NewLine, rooms.Single(r => r.Id == id).Patients.Select(p => p.Name).OrderBy(p => p));
+    }
+}
+class Program
+{
+    private static List<dep> departments = new List<dep>();
+    private static List<doc> doctors = new List<doc>();
+
+    static void Main(string[] args)
+    {
+        string command;
+
+        while ((command = Console.ReadLine()) != "Output")
+        {
+            var commandArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            ParseCommand(commandArgs);
+        }
+
+        while ((command = Console.ReadLine()) != "End")
+        {
+            var commandArgs = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            PrintOutput(commandArgs);
+        }
+    }
+
+    private static void PrintOutput(string[] commandArgs)
+    {
+        if (commandArgs.Length == 1)
+        {
+            string departmentName = commandArgs[0];
+
+            Console.WriteLine(departments.Single(d => d.Name == departmentName).GetAllPatients());
+        }
+        else if (commandArgs.Length == 2)
+        {
+            if (commandArgs[1].All(Char.IsDigit))
+            {
+                string departmentName = commandArgs[0];
+                int roomNumber = int.Parse(commandArgs[1]);
+
+                Console.WriteLine(departments.Single(d => d.Name == departmentName).GetPatientsByRoom(roomNumber));
+            }
+            else
+            {
+                string doctorFirstName = commandArgs[0];
+                string doctorLastName = commandArgs[1];
+
+                Console.WriteLine(doctors.Single(d => d.FirstName == doctorFirstName && d.LastName == doctorLastName).GetPatients());
+            }
+        }
+    }
+
+    private static void ParseCommand(string[] commandArgs)
+    {
+        string departmentName = commandArgs[0];
+        string doctorFirstName = commandArgs[1];
+        string doctorLastName = commandArgs[2];
+        string patientName = commandArgs[3];
+
+        if (!departments.Any(d => d.Name == departmentName))
+        {
+            departments.Add(new dep(departmentName));
+        }
+
+        var department = departments.Single(d => d.Name == departmentName);
+        var patient = new patient(patientName);
+
+        department.AddPatientToRoom(patient);
+
+        if (!doctors.Any(d => d.FirstName == doctorFirstName && d.LastName == doctorLastName))
+        {
+            doctors.Add(new doc(doctorFirstName, doctorLastName));
+        }
+
+        var doctor = doctors.Single(d => d.FirstName == doctorFirstName && d.LastName == doctorLastName);
+
+        doctor.AddPatient(patient);
     }
 }
--- laba4/5/Program.cs
+++ laba4/5/Program.cs
@@ -1,12 +1,215 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
-namespace _5
+public class bag
 {
-    class Program
+    private List<Item> bag1;
+    private long capacity;
+    private long current;
+
+    public bag(long capacity)
     {
-        static void Main(string[] args)
+        this.capacity = capacity;
+        bag1 = new List<Item>();
+    }
+
+    public long GoldItemsValue
+    {
+        get
         {
-            Console.WriteLine("Hello World!");
+            return bag1.Where(i => i is gold).Sum(i => i.Value);
+        }
+    }
+
+    public long CashItemsValue
+    {
+        get
+        {
+            return bag1.Where(i => i is cash).Sum(i => i.Value);
+        }
+    }
+
+    public long GemItemsValue
+    {
+        get
+        {
+            return bag1.Where(i => i is gem).Sum(i => i.Value);
+        }
+    }
+
+    public void AddGoldItem(gold item)
+    {
+        if (capacity >= current + item.Value)
+        {
+            var goldItems = GetGoldItems();
+            if (goldItems.Any(gi => gi.Key == item.Key))
+            {
+                goldItems.Single(gi => gi.Key == item.Key).IncreaseValue(item.Value);
+            }
+            else
+            {
+                bag1.Add(item);
+            }
+
+            current += item.Value;
+        }
+    }
+
+    public void AddGemItem(gem item)
+    {
+        if (capacity >= current + item.Value && GoldItemsValue >= GemItemsValue + item.Value)
+        {
+            var gemItems = GetGemItems();
+            if (gemItems.Any(gi => gi.Key == item.Key))
+            {
+                gemItems.Single(gi => gi.Key == item.Key).IncreaseValue(item.Value);
+            }
+            else
+            {
+                bag1.Add(item);
+            }
+            current += item.Value;
+        }
+    }
+
+    public void AddCashItem(cash item)
+    {
+        if (capacity >= current + item.Value && GemItemsValue >= CashItemsValue + item.Value)
+        {
+            var cashItems = GetCashItems();
+            if (cashItems.Any(gi => gi.Key == item.Key))
+            {
+                cashItems.Single(gi => gi.Key == item.Key).IncreaseValue(item.Value);
+            }
+            else
+            {
+                bag1.Add(item);
+            }
+            current += item.Value;
+        }
+    }
+
+    public List<Item> GetCashItems()
+    {
+        return bag1.Where(i => i is cash).ToList();
+    }
+
+    public List<Item> GetGoldItems()
+    {
+        return bag1.Where(i => i is gold).ToList();
+    }
+
+    public List<Item> GetGemItems()
+    {
+        return bag1.Where(i => i is gem).ToList();
+    }
+
+    public override string ToString()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        var dictionary = bag1.GroupBy(i => i.GetType().Name).ToDictionary(g => g.Key, g => g.ToList());
+
+        foreach (var kvp in dictionary.OrderByDescending(kv => kv.Value.Sum(i => i.Value)))
+        {
+            if (kvp.Key == "CashItem")
+            {
+                builder.AppendLine($"<Cash> ${kvp.Value.Sum(i => i.Value)}");
+            }
+            else if (kvp.Key == "GemItem")
+            {
+                builder.AppendLine($"<Gem> ${kvp.Value.Sum(i => i.Value)}");
+            }
+            else if (kvp.Key == "GoldItem")
+            {
+                builder.AppendLine($"<Gold> ${kvp.Value.Sum(i => i.Value)}");
+            }
+
+            foreach (var item in kvp.Value.OrderByDescending(i => i.Key).ThenBy(i => i.Value))
+            {
+                builder.AppendLine($"##{item.Key} - {item.Value}");
+            }
+        }
+
+        return builder.ToString().TrimEnd();
+    }
+}
+
+
+public class cash : Item
+{
+    public cash(string key, long value)
+    {
+        Key = key;
+        Value = value;
+    }
+}
+
+public class gem : Item
+{
+    public gem(string key, long value)
+    {
+        Key = key;
+        Value = value;
+    }
+}
+public class gold : Item
+{
+    public gold(string key, long value)
+    {
+        Key = key;
+        Value = value;
+    }
+}
+public abstract class Item
+{
+    public string Key { get; protected set; }
+    public long Value { get; protected set; }
+
+    public void IncreaseValue(long value)
+    {
+        Value += value;
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        long capacity = long.Parse(Console.ReadLine());
+        var input = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        var bag = new bag(capacity);
+
+        for (int i = 0; i < input.Length; i += 2)
+        {
+            string key = input[i];
+            long value = long.Parse(input[i + 1]);
+
+            InsertItem(key, value, bag);
+        }
+
+        Console.WriteLine(bag.ToString());
+    }
+
+    private static void InsertItem(string key, long value, bag bag)
+    {
+        if (key.Length == 3)
+        {
+            cash cash = new cash(key, value);
+            bag.AddCashItem(cash);
+        }
+        else if (key.Length >= 4 && key.ToLower().EndsWith("gem"))
+        {
+            gem gem = new gem(key, value);
+            bag.AddGemItem(gem);
+        }
+        else if (key.ToLower().Equals("gold"))
+        {
+            gold gold = new gold(key, value);
+            bag.AddGoldItem(gold);
         }
     }
 }
