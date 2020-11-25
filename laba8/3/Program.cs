19ddc1dc9b5f68001c217face31a611ed9a46f98
Andriy68 <osinskyy.andriy@chnu.edu.ua>
2020-11-25 22:01:39 +0200

    9

--- laba8/3/Program.cs
+++ laba8/3/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 
-namespace _03._Shapes
+
+
+
+namespace _3_Shapes
+
 {
+
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+
+            Shape rectangle = new Rectangle(5, 5);
+
+            Console.WriteLine($"Area: {rectangle.CalculateArea()}");
+
+            Console.WriteLine($"Perimeter: {rectangle.CalculatePerimeter()}");
+
+            Console.WriteLine(rectangle.Draw());
+
+
+
+
+            Shape circle = new Circle(3);
+
+            Console.WriteLine($"Area: {circle.CalculateArea()}");
+
+            Console.WriteLine($"Perimeter: {circle.CalculatePerimeter()}");
+
+            Console.WriteLine(circle.Draw());
+
         }
+
     }
+
 }
